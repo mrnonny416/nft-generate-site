c494c6b215a7b784310b8d270cd63eddbdf3a27d
mrnonny <68236769+mrnonny416@users.noreply.github.com>
2024-07-03 17:07:18 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import HomePage from "@/src/home/HomePage";
+
 export default function Home() {
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            suraphong.qiin.work
+        <div className="flex flex-row justify-center bg-black">
+            <HomePage />
         </div>
     );
 }
---
+++ src/home/HomePage.tsx
@@ -0,0 +1,27 @@
+export default function HomePage() {
+    return (
+        <div
+            className="flex flex-col w-full h-screen  max-w-[400px]"
+            style={{
+                backgroundImage: "url(/image.png)",
+                backgroundSize: "cover",
+                backgroundPosition: "bottom",
+                backgroundRepeat: "no-repeat"
+            }}
+        >
+            <div className="flex flex-row justify-between text-[#CCFF00] px-3 bg-black pt-10">
+                <div className="flex text-[34px]">NFT</div>
+                <button className="flex border border-2 border-[#CCFF00] px-[10px] py-[7px] text-[18px] rounded-xl">
+                    Connect
+                </button>
+            </div>
+            <div className="flex flex-col w-full px-3 h-screen bg-gradient-to-b from-black from-80% to-black-1/6  gap-10 max-w-[400px]">
+                <div className="text-[67px]">CREATE YOUR OWN NFT FROM OUR TOOLS</div>
+                <div className="text-[26px]">สร้าง NFT ของคุณง่ายๆ ด้วยเครื่องมือของเรา ไม่กี่ขั้นตอน</div>
+                <button className="px-[10px] py-[7px] text-[18px] w-fit border border-2 border-[#CCFF00] bg-[#CCFF00] text-black rounded-xl">
+                    Get Started
+                </button>
+            </div>
+        </div>
+    );
+}
