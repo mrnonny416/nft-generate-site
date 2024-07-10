00158921f424b1615cf778ecc3b6b57fdb3799c8
mrnonny416 <mrnonny416@gmail.com>
2024-07-10 10:40:13 +0700

    feat [file] : update file name

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,24 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import DashboardPage from "@/src/dashboard/dashvoard.page";
-import DashboardItemsPage from "@/src/dashboard/dashvoard.items.page";
-
+import DashboardPage from "@/src/dashboard/dashboard.page";
+import DashboardItemsPage from "@/src/dashboard/dashboard.items.page";
 
 export default function Dashboard(params: any) {
-  const value = params.searchParams.item
-  const [chkTypePage, setChkTypePage] = useState<boolean>(true);
+    const value = params.searchParams.item;
+    const [chkTypePage, setChkTypePage] = useState<boolean>(true);
 
-  console.log("value", value);
+    console.log("value", value);
 
-  useEffect(() => {
-    if (value) {
-      setChkTypePage(false)
-    }
-  }, [value])
+    useEffect(() => {
+        if (value) {
+            setChkTypePage(false);
+        }
+    }, [value]);
 
-
-  return (
-    <div className="flex flex-row justify-center bg-black">
-      {chkTypePage ? <DashboardPage typePageDashboard={() => setChkTypePage(false)} /> : <DashboardItemsPage />}
-    </div>
-  );
+    return (
+        <div className="flex flex-row justify-center bg-black">
+            {chkTypePage ? <DashboardPage typePageDashboard={() => setChkTypePage(false)} /> : <DashboardItemsPage />}
+        </div>
+    );
 }
